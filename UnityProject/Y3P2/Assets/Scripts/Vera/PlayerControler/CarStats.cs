124442883489a57b85f2324c33e8288a8e8b4b76
VeraGerritse <anna.vera.gerritse@gmail.com>
2018-11-21 11:45:25 +0100

    folder structure

---
+++ UnityProject/Y3P2/Assets/Scripts/Vera/PlayerControler/CarStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarStats : MonoBehaviour {
+    public float speed;
+    public float weight;
+    public float accelaration;
+    public float handling;
+}
---
+++ UnityProject/Y3P2/Assets/Scripts/Vera/PlayerControler/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public CarStats myCar;
+    public float hor;
+    public float vert;
+    public int playerNum = 1;
+
+    float currentSpeed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        hor = Input.GetAxis("C" + playerNum.ToString() + " Hor") * myCar.speed;
+        transform.Rotate(0, 0, hor);
+    }
+}
